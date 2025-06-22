60971edf5644d33297780ef702da6624d4afb7ab
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-06-22 21:55:47 +0100

    spring initializr

---
+++ src/main/java/com/abrahambankole/stridedotcom/StridedotcomApplication.java
@@ -0,0 +1,13 @@
+package com.abrahambankole.stridedotcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StridedotcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StridedotcomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abrahambankole/stridedotcom/StridedotcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abrahambankole.stridedotcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StridedotcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
