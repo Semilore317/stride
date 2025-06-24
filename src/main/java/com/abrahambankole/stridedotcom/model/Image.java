db1d651977d1b965dca2802a316c1e4e0aaabcc4
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-06-25 00:38:25 +0100

    scaffolded image entity with lombok annotations

--- src/main/java/com/abrahambankole/stridedotcom/model/Image.java
+++ src/main/java/com/abrahambankole/stridedotcom/model/Image.java
@@ -1,5 +1,31 @@
 package com.abrahambankole.stridedotcom.model;
 
-public class Image {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.sql.Blob;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String filename;
+    private String fileType;
+    @Lob
+    private Blob image; // Binary Large Object for image, CLOB - Character Large Object for text
+
+    private String downloadUrl;
+
+    // set up relationship between image and product - one product to many images
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 }
