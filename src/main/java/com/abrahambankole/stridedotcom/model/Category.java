88bd5360af0c623942aa45fab71f3cc1dc0fdaa5
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-06-25 00:29:52 +0100

    specified lombok annotations for category enitity

--- src/main/java/com/abrahambankole/stridedotcom/model/Category.java
+++ src/main/java/com/abrahambankole/stridedotcom/model/Category.java
@@ -1,4 +1,27 @@
 package com.abrahambankole.stridedotcom.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+    // only made a constructor for name because we're not adding for products
+    public Category(String name) {
+        this.name = name;
+    }
 }
