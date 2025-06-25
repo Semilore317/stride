dd55b9f81f4037fbed65fbfbe4238aff4a465a0f
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-06-25 01:22:42 +0100

    added Role entity implementation

---
+++ src/main/java/com/abrahambankole/stridedotcom/model/Cart.java
@@ -0,0 +1,4 @@
+package com.abrahambankole.stridedotcom.model;
+
+public class Cart {
+}
--- src/main/java/com/abrahambankole/stridedotcom/model/Role.java
+++ src/main/java/com/abrahambankole/stridedotcom/model/Role.java
@@ -5,20 +5,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
-public class Category {
+public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
 
-    //could technically use a list instead but who cares?
+
+    // relationship between role and user - many users to many roles
+    // collection is easier to swap than lists or sets but either should work
     @ManyToMany(mappedBy = "roles")
     private Collection<User> users = new HashSet<>();
+
+    public Role(String name) {
+        this.name = name;
+    }
 }
