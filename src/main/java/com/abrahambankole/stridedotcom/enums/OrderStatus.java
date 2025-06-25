a1cabd08062245b5fae83e7c40ab53e366d5a61c
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-06-25 01:26:28 +0100

    defined enums for OrderStatus

---
+++ src/main/java/com/abrahambankole/stridedotcom/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.abrahambankole.stridedotcom.enums;
+
+public enum OrderStatus {
+    // what states are there for an order?
+    PENDING,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED,
+}
--- src/main/java/com/abrahambankole/stridedotcom/model/Order.java
+++ src/main/java/com/abrahambankole/stridedotcom/model/Order.java
@@ -1,4 +1,11 @@
 package com.abrahambankole.stridedotcom.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Order {
+    private Long orderId;
+    private LocalDate orderDate;
+    private BigDecimal totalAmount;
+    private OrderStatus orderStatus;
 }
