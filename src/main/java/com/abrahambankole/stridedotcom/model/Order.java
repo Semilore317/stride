71b24b5d6f327af355d9cebb794eb1580e4dfd3b
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-06-25 01:29:00 +0100

    defined Order entity

--- src/main/java/com/abrahambankole/stridedotcom/model/Order.java
+++ src/main/java/com/abrahambankole/stridedotcom/model/Order.java
@@ -1,11 +1,30 @@
 package com.abrahambankole.stridedotcom.model;
 
+import com.abrahambankole.stridedotcom.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
     private LocalDate orderDate;
     private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
