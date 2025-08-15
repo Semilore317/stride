b3f6286fb79949b365dff43c5bf3ab0d3120d51c
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-15 17:57:40 +0100

    set up use entity relation

---
+++ src/main/java/com/abraham_bankole/stridedotcom/model/Cart.java
@@ -0,0 +1,25 @@
+package com.abraham_bankole.stridedotcom.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal totalAmount;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
