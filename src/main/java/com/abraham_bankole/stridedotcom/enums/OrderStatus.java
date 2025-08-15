c4b3488c4f1b3036c6d8d5e3a719ab5e4130714d
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-15 17:57:40 +0100

    set up use entity relation

---
+++ src/main/java/com/abraham_bankole/stridedotcom/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.abraham_bankole.stridedotcom.enums;
+
+public enum OrderStatus {
+    PENDING,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED,
+}
---
+++ src/main/java/com/abraham_bankole/stridedotcom/model/Order.java
@@ -0,0 +1,29 @@
+package com.abraham_bankole.stridedotcom.model;
+
+import com.abraham_bankole.stridedotcom.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+    private LocalDate orderDate;
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
