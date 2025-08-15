3238323209030fb94c0c25d55f5bfdc8a1f19e88
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-15 20:34:57 +0100

    implemented the order entity relation

---
+++ src/main/java/com/abraham_bankole/stridedotcom/model/OrderItem.java
@@ -0,0 +1,40 @@
+package com.abraham_bankole.stridedotcom.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int quantity;
+    private BigDecimal price;
+
+    // Relationships
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    // constuctor
+
+    public OrderItem(int quantity, BigDecimal price, Order order, Product product) {
+        this.quantity = quantity;
+        this.price = price;
+        this.order = order;
+        this.product = product;
+    }
+}
