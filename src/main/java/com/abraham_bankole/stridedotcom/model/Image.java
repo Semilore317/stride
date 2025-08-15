12673ab812fe77469ae86c3dcbf9dd9feaf3af09
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-15 14:16:42 +0100

    implemented the entity classes...

--- src/main/java/com/abraham_bankole/stridedotcom/model/Image.java
+++ src/main/java/com/abraham_bankole/stridedotcom/model/Image.java
@@ -1,4 +1,30 @@
 package com.abraham_bankole.stridedotcom.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+    @Lob
+    private Blob image; // Binary Large OBject
+
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 }
