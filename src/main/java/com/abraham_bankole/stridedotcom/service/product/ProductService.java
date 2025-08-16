137ac62edb1a07638cea1c0b9a7812d9981ec56c
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-16 09:32:58 +0100

    implemented the product sevice layer

--- src/main/java/com/abraham_bankole/stridedotcom/service/product/ProductService.java
+++ src/main/java/com/abraham_bankole/stridedotcom/service/product/ProductService.java
@@ -1,4 +1,65 @@
 package com.abraham_bankole.stridedotcom.service.product;
 
-public class ProductService {
+import com.abraham_bankole.stridedotcom.model.Product;
+import com.abraham_bankole.stridedotcom.repository.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor //generates a constructor for only the final or @NonNull fields
+public class ProductService implements iProductService {
+    private final ProductRepository productRepository;
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product updateProduct(Product product, Long productId) {
+        return null;
+    }
+
+    @Override
+    public Product getProductsById(Long productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new EntityNotFoundException("Product Not Found"));
+    }
+
+    @Override
+    public void deleteProduct(Long productId) {
+
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return productRepository.findByName(name);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return productRepository.findByCategoryName(category);
+    }
+
+    @Override
+    public List<Product> getProductsByBrand(String brand) {
+        return productRepository.findByBrand(brand);
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+        return productRepository.findByCategoryNameAndBrand(category, brand);
+    }
+
+    @Override
+    public List<Product> getProductsByBrandAndName(String brand, String name) {
+        return productRepository.findByNameAndBrand(brand, name);
+    }
 }
