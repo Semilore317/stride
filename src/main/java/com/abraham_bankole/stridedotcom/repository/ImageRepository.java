f42ead00184d7124a23c69f45ef8a78d587e8997
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-17 17:45:54 +0100

    successfully implemented the image Services without bugs

---
+++ src/main/java/com/abraham_bankole/stridedotcom/repository/ImageRepository.java
@@ -0,0 +1,8 @@
+package com.abraham_bankole.stridedotcom.repository;
+
+import com.abraham_bankole.stridedotcom.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
