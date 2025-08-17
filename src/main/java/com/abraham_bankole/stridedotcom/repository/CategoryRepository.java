9c1273d463b6251257c2d601a508c30cb6c12679
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-17 15:04:53 +0100

    fixed a shitty bug, changes signature of findByName method from Category to String

--- src/main/java/com/abraham_bankole/stridedotcom/repository/CategoryRepository.java
+++ src/main/java/com/abraham_bankole/stridedotcom/repository/CategoryRepository.java
@@ -4,5 +4,7 @@ import com.abraham_bankole.stridedotcom.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Category findByName(Category category);
+    Category findByName(String category); // if there's an error, its probably here - String vs Object
+
+
 }
