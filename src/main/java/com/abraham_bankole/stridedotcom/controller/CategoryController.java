a504d21e971b60d450d823cf43d396d5dee3e8e6
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-18 12:41:13 +0100

    implemented the category controller...

--- src/main/java/com/abraham_bankole/stridedotcom/controller/CategoryController.java
+++ src/main/java/com/abraham_bankole/stridedotcom/controller/CategoryController.java
@@ -1,4 +1,53 @@
 package com.abraham_bankole.stridedotcom.controller;
 
+import com.abraham_bankole.stridedotcom.model.Category;
+import com.abraham_bankole.stridedotcom.response.ApiResponse;
+import com.abraham_bankole.stridedotcom.service.category.iCategoryService;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/categories") // best practice is to pluralize the item
 public class CategoryController {
+    private final iCategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories() {
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Found", categories));
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse("Error: ", e.getMessage()));
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addCategoryById(@RequestBody Category category) {
+        try {
+            Category theCategory = categoryService.addCategory(category);
+            return ResponseEntity.ok(new ApiResponse("Successfully Added", theCategory));
+        } catch (EntityExistsException e) {
+            return ResponseEntity.status(CONFLICT).body(new ApiResponse("Error: ", e.getMessage()));
+        }
+    }
+
+    @GetMapping("/category/{id}/category")
+    public ResponseEntity<ApiResponse> getCategoryById(@PathVariable Long id) {
+        try {
+            Category theCategory = categoryService.findCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Successfully Found", theCategory));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse("Error: ", e.getMessage()));
+        }
+    }
 }
