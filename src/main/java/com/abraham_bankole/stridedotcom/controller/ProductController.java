d2e7435c200ec412c8d13d9d370c6584ee9a76e0
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-18 22:19:00 +0100

    made adjustments

--- src/main/java/com/abraham_bankole/stridedotcom/controller/ProductController.java
+++ src/main/java/com/abraham_bankole/stridedotcom/controller/ProductController.java
@@ -5,13 +5,18 @@ import com.abraham_bankole.stridedotcom.model.Product;
 import com.abraham_bankole.stridedotcom.repository.ProductRepository;
 import com.abraham_bankole.stridedotcom.response.ApiResponse;
 import com.abraham_bankole.stridedotcom.service.product.iProductService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.engine.spi.Resolution;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +30,16 @@ public class ProductController {
         List<Product> products = productService.getAllProducts();
         List<ProductDto> convertedProducts = productService.getConvertedProducts(products);
         return ResponseEntity.ok(new ApiResponse("Found", convertedProducts));
+    }
+
+    @GetMapping("/product/{productId}/product")
+    public ResponseEntity<ApiResponse> getProductById(@PathVariable Long productId) {
+        Product product = productService.getProductsById(productId);
+        try {
+            ProductDto productDto = productService.convertToDto(product);
+            return ResponseEntity.ok(new ApiResponse("Found!", productDto));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse("Oops!", e));
+        }
     }
 }
