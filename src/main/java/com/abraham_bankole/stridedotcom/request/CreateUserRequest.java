1ba8faa30c46f17737d08cef2cb1fb1b9974e75b
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-22 15:01:08 +0100

    implemented the user service

--- src/main/java/com/abraham_bankole/stridedotcom/request/CreateUserRequest.java
+++ src/main/java/com/abraham_bankole/stridedotcom/request/CreateUserRequest.java
@@ -1,4 +1,12 @@
 package com.abraham_bankole.stridedotcom.request;
 
+import lombok.Data;
+
+@Data
 public class CreateUserRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
 }
