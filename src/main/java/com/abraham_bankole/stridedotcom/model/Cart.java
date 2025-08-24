eb178672d65dd6064a2831889852883882cd3993
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-24 21:33:37 +0100

    fixed most of the issues i encountered when testing with Postmanm, still some work to be done - getting StackOverflowError

--- src/main/java/com/abraham_bankole/stridedotcom/model/Cart.java
+++ src/main/java/com/abraham_bankole/stridedotcom/model/Cart.java
@@ -1,5 +1,6 @@
 package com.abraham_bankole.stridedotcom.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
--- src/main/java/com/abraham_bankole/stridedotcom/model/CartItem.java
+++ src/main/java/com/abraham_bankole/stridedotcom/model/CartItem.java
@@ -17,12 +17,11 @@ public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private int  quantity;
+    private int quantity;
     private BigDecimal unitPrice;
     private BigDecimal totalPrice;
 
-    // relationships
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
 
@@ -30,7 +29,7 @@ public class CartItem {
     @JoinColumn(name = "cart_id")
     private Cart cart;
 
-    public void setTotalPrice() {
+    public  void setTotalPrice(){
         this.totalPrice = this.unitPrice.multiply(new BigDecimal(quantity));
     }
 }
