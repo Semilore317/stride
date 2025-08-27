9c4d55e1435e2e52a54feb12cd98214f3a1f6810
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-27 22:05:29 +0100

    created and implemented the user security classes

---
+++ src/main/java/com/abraham_bankole/stridedotcom/security/user/ShopUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.abraham_bankole.stridedotcom.security.user;
+
+import com.abraham_bankole.stridedotcom.model.User;
+import com.abraham_bankole.stridedotcom.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ShopUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = Optional.ofNullable(userRepository.findByEmail(email))
+                .orElseThrow(() -> new EntityNotFoundException("User Not Found"));
+        return ShopUserDetails.buildUserDetails(user);
+    }
+}
