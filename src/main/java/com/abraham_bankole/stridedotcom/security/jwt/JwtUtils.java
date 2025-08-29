cb538cacb9af09d79372b09b3178555382434e44
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-29 11:33:38 +0100

    implemented JWT Utils methods

--- src/main/java/com/abraham_bankole/stridedotcom/security/jwt/JwtUtils.java
+++ src/main/java/com/abraham_bankole/stridedotcom/security/jwt/JwtUtils.java
@@ -1,4 +1,87 @@
 package com.abraham_bankole.stridedotcom.security.jwt;
 
-public class JwtUtils {
+
+import com.abraham_bankole.stridedotcom.security.user.ShopUserDetails;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+
+@Component
+public class JwtUtils{
+    @Value("${auth.token.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${auth.token.access-expiration-in-millis}")
+    private String expirationTime;    // for some reason, i used String and not Long
+
+    @Value("${auth.token.refresh-expiration-in-millis}")
+    private String refreshExpirationTime; // for some reason, i used String and not Long
+
+    private Date calculateExpiration(String expirationTimeString) {
+        long expirationTime = Long.parseLong(expirationTimeString);
+        return new Date(System.currentTimeMillis() + expirationTime);
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String generateAccessTokenForUser(Authentication authentication) {
+        ShopUserDetails userPrincipal = (ShopUserDetails) authentication.getPrincipal();
+
+        List<String> roles = userPrincipal.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority).toList();
+
+        return Jwts.builder()
+                .setSubject(userPrincipal.getEmail())
+                .claim("id", userPrincipal.getId())
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(calculateExpiration(expirationTime))
+                .signWith(key(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateRefreshToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(calculateExpiration(refreshExpirationTime))
+                .signWith(key(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getUserNameFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try{
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        }catch (JwtException e){
+            throw new JwtException(e.getMessage());
+        }
+    }
+
 }
