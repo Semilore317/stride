5c67947c2f37c7d64ce9346258c26a59481961a8
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-29 16:06:05 +0100

    implemented JWT services

--- src/main/java/com/abraham_bankole/stridedotcom/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/abraham_bankole/stridedotcom/security/jwt/AuthTokenFilter.java
@@ -1,4 +1,53 @@
 package com.abraham_bankole.stridedotcom.security.jwt;
 
-public class AuthTokenFilter {
+import com.abraham_bankole.stridedotcom.security.user.ShopUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class AuthTokenFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private ShopUserDetailsService UserDetailsService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        try{
+            String jwt = parseJwt(request);
+            if(StringUtils.hasText(jwt) && jwtUtils.validateToken(jwt)) {
+                String username = jwtUtils.getUserNameFromToken(jwt);
+                UserDetails userDetails = UserDetailsService.loadUserByUsername(username);
+                // actual data type is UsernamePasswordAuthenticationToken implementing the Authentication interface but i used var for  clarity
+                var auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }catch(Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    public String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7);
+        }
+        return null;
+    }
 }
