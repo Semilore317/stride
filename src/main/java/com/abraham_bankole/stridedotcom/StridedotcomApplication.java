36cb87c8e955bddd39f8cf218b137e6a92544b9b
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-29 20:21:51 +0100

    completed AuthController and implemented the security config classes

--- src/main/java/com/abraham_bankole/stridedotcom/StridedotcomApplication.java
+++ src/main/java/com/abraham_bankole/stridedotcom/StridedotcomApplication.java
@@ -3,6 +3,7 @@ package com.abraham_bankole.stridedotcom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class StridedotcomApplication {
 	public static void main(String[] args) {
