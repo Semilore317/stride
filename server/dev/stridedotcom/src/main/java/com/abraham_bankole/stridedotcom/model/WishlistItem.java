20e0d0c3af3b15b9fa08119995d8056a7f9b0b58
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-09-06 00:10:16 +0100

    implemented business logic for wishlist and wishlistitem - repos and services

--- server/dev/stridedotcom/src/main/java/com/abraham_bankole/stridedotcom/model/WishlistItem.java
+++ server/dev/stridedotcom/src/main/java/com/abraham_bankole/stridedotcom/model/WishlistItem.java
@@ -1,8 +1,12 @@
 package com.abraham_bankole.stridedotcom.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class WishlistItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
