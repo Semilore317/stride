7579b9593b502c95cce31a4fd54bacf4a084a831
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-09-06 22:31:29 +0100

    ..

--- server/dev/stridedotcom/src/main/java/com/abraham_bankole/stridedotcom/model/Wishlist.java
+++ server/dev/stridedotcom/src/main/java/com/abraham_bankole/stridedotcom/model/Wishlist.java
@@ -2,6 +2,8 @@ package com.abraham_bankole.stridedotcom.model;
 
 import com.abraham_bankole.stridedotcom.model.Product;
 import com.abraham_bankole.stridedotcom.model.User;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,11 +15,12 @@ import java.util.List;
 @Getter
 @Setter
 public class Wishlist {
-
+//
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonIgnore
     @OneToOne
     @JoinColumn(name = "user_id", unique = true)
     private User user;
