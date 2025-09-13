3d39a94eb78c94ff13169e2988deec717fe56569
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-09-13 20:13:54 +0100

    fixed searchBar

--- client/dev/stridedotcom/src/store/features/SearchSlice.js
+++ client/dev/stridedotcom/src/store/features/SearchSlice.js
@@ -1,22 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+    searchQuery: "",
+    selectedCategory: "all",
+};
+
 const searchSlice = createSlice({
     name: "search",
-
-    initialState: {
-        query: "", // default search query is an emptty string
-    },
-
+    initialState,
     reducers: {
-        setSearchQuery: (state, action) => {
-            state.query = action.payload;
+        setSearchQuery(state, action) {
+            state.searchQuery = action.payload;
         },
-        clearSearchQuery: (state) => {
-            state.query = "";
+        setSelectedCategory(state, action) {
+            state.selectedCategory = action.payload;
         },
     },
 });
 
-export const { setSearchQuery, clearSearchQuery } = searchSlice.actions;
-
+export const { setSearchQuery, setSelectedCategory } = searchSlice.actions;
 export default searchSlice.reducer;
