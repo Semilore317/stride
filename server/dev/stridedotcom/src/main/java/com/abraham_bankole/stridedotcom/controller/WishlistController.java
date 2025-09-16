7e265d61ae1d3098037d9446ee8a13cc8c1708ab
Abraham Bankole <112177168+Semilore317@users.noreply.github.com>
2025-09-16 23:12:41 +0100

    Update WishlistController.java

--- server/dev/stridedotcom/src/main/java/com/abraham_bankole/stridedotcom/controller/WishlistController.java
+++ server/dev/stridedotcom/src/main/java/com/abraham_bankole/stridedotcom/controller/WishlistController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// TODO: Test endpoints in Postman 
+
 @RestController
 @RequestMapping("${api.prefix}/wishlist")
 @RequiredArgsConstructor
