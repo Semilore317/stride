3cb56055acb83a0bc98cd7052243f69860c3881a
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-10-08 15:43:32 +0100

    implemented the category slice and fixed bugs

--- client/dev/stridedotcom/src/store/store.js
+++ client/dev/stridedotcom/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "./features/SearchSlice.js";
+import categoryReducer from "./features/CategorySlice.js"
 
 export const store = configureStore({
   reducer: {
-    search: searchReducer
+    search: searchReducer,
+    category: categoryReducer,
   },
 });
