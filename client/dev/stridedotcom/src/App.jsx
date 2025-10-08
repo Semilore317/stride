9fa5982ce20b4926e7b9756447a74f72452bba6c
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-10-08 21:04:08 +0100

    worked on all products page

--- client/dev/stridedotcom/src/App.jsx
+++ client/dev/stridedotcom/src/App.jsx
@@ -2,12 +2,14 @@ import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} fr
 import RootLayout from "@/components/layout/RootLayout.jsx";
 import 'react-toastify/dist/ReactToastify.css';
 import Home from "@/components/home/Home.jsx";
+import Products from './components/product/Products';
 
 function App() {
     const router = createBrowserRouter(
         createRoutesFromElements(
             <Route path="/" element={<RootLayout/>}>
                 <Route index element={<Home />} />
+                <Route path="/products" element={<Products />} />
             </Route>
         )
     )
