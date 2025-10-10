ecd17d135e9136459b4a7ca046aca00b51920e03
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-10-10 21:49:44 +0100

    db stuff

--- server/dev/stridedotcom/src/main/java/com/abraham_bankole/stridedotcom/controller/ProductController.java
+++ server/dev/stridedotcom/src/main/java/com/abraham_bankole/stridedotcom/controller/ProductController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 @RequiredArgsConstructor
