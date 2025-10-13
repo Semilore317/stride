680212254554b626653e52419552b1a4c713e4ce
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-10-13 15:27:19 +0100

    worked on productdetails page and subtle ui elements

--- client/dev/stridedotcom/src/App.jsx
+++ client/dev/stridedotcom/src/App.jsx
@@ -1,18 +1,29 @@
-import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import RootLayout from "@/components/layout/RootLayout.jsx";
 import 'react-toastify/dist/ReactToastify.css';
 import Home from "@/components/home/Home.jsx";
 import Products from './components/product/Products';
+import ProductDetails from './components/product/ProductDetails';
 
 function App() {
-    const router = createBrowserRouter(
-        createRoutesFromElements(
-            <Route path="/" element={<RootLayout/>}>
-                <Route index element={<Home />} />
-                <Route path="/products" element={<Products />} />
-            </Route>
-        )
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path='/products/:name' element={<ProductDetails />} />
+        {/* optional catch-all */}
+        <Route path="*" element={
+          <div>
+            <h1>
+              404 - Not Found
+            </h1>
+              <p>Sorry, the page you are looking for does not exist.</p>
+          </div>
+          } />
+      </Route>
     )
+  )
   return (
     <RouterProvider router={router} />
   )
---
+++ client/dev/stridedotcom/src/components/common/LoadSpinner.jsx
@@ -0,0 +1,7 @@
+const LoadSpinner = () => (
+    <div className="flex justify-center items-center h-screen">
+        <div className="w-12 h-12 border-4 border-gray-300 border-t-gray-800 rounded-full animate-spin"></div>
+    </div>
+);
+
+export default LoadSpinner;
