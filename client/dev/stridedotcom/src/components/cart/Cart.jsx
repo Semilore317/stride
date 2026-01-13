f4f8b3062108a067e39f8ec468c924240881b318
Abraham Bankole <semi.banks.317@gmail.com>
2026-01-13 14:45:23 +0100

    cart component structure

---
+++ client/dev/stridedotcom/src/components/cart/Cart.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <>
+      {/*
+        //stuff to add to the cart section
+        1. product image
+        2. product name
+        3. brand name
+        4. unit price
+        5. quantity selector
+        6. total price
+        7. actionas
+
+        */}
+      <table>
+        <thead>
+          <tr></tr>
+        </thead>
+      </table>
+    </>
+  );
+};
+
+export default Cart;
