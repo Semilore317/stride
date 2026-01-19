965782e4e695b85e0fcfc33aa71ce9595319f7da
Abraham Bankole <semi.banks.317@gmail.com>
2026-01-19 09:40:25 +0100

    implemented the user address services

--- server/dev/stridedotcom/src/main/java/com/abraham_bankole/stridedotcom/service/address/AddressService.java
+++ server/dev/stridedotcom/src/main/java/com/abraham_bankole/stridedotcom/service/address/AddressService.java
@@ -2,6 +2,7 @@ package com.abraham_bankole.stridedotcom.service.address;
 
 import com.abraham_bankole.stridedotcom.model.Address;
 import com.abraham_bankole.stridedotcom.repository.AddressRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,29 +11,46 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class AddressService implements IAddressService {
-    private AddressRepository addressRepository;
+
+    private final AddressRepository addressRepository;
+
     @Override
     public List<Address> createAddress(List<Address> addressList) {
-        return List.of();
+        return addressRepository.saveAll(addressList);
     }
 
     @Override
     public List<Address> getUserAddress(Long userId) {
-        return List.of();
+        return addressRepository.findByUserId(userId);
     }
 
     @Override
     public Address getAddressById(Long addressId) {
-        return null;
+        return addressRepository.findById(addressId)
+                .orElseThrow(() -> new EntityNotFoundException("Address Not Found"));
     }
 
     @Override
     public void deleteAddress(Long addressId) {
-
+        addressRepository.findById(addressId).ifPresentOrElse(
+                addressRepository::delete,
+                () -> {
+                    throw new EntityNotFoundException("Address Not Found")
+                }
+        );
     }
 
     @Override
     public Address updateUserAddress(Long id, Address address) {
-        return null;
+        return addressRepository.findById(id).map(existingAddress -> {
+            existingAddress.setCountry(address.getCountry());
+            existingAddress.setState(address.getState());
+            existingAddress.setCity(address.getCity());
+            existingAddress.setAddressType(address.getAddressType());
+
+            return addressRepository.save(existingAddress);
+        }).orElseThrow(
+                () -> new EntityNotFoundException("Address Not Found!")
+        );
     }
 }
