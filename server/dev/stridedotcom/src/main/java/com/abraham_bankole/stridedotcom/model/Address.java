a8f8df9c2900ee7e96c053b00eb3f75f60078148
Abraham Bankole <semi.banks.317@gmail.com>
2026-01-19 08:43:04 +0100

    implemented address entity and entity relations with user

---
+++ server/dev/stridedotcom/src/main/java/com/abraham_bankole/stridedotcom/model/Address.java
@@ -0,0 +1,30 @@
+package com.abraham_bankole.stridedotcom.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String country;
+    private String state;
+    private String city;
+    private String street;
+
+    @Enumerated(EnumType.STRING)
+    private AddressType addressType;
+
+    // user relationship with address
+    @ManyToOne // single user can have multiple addresses
+    @JoinColumn(name = "user_id")
+    private User user;
+}
