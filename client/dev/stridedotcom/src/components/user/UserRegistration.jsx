bf6faa4dfa37b1aa274a384cd01640a604c9b01a
Abraham Bankole <semi.banks.317@gmail.com>
2026-01-20 09:42:06 +0100

    worked on functions for the user registration component

--- client/dev/stridedotcom/src/components/user/UserRegistration.jsx
+++ client/dev/stridedotcom/src/components/user/UserRegistration.jsx
@@ -1,12 +1,47 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useDispatch} from "react-redux";
 import {getUserById} from "@/store/features/userSlice.js";
 
 const UserRegistration = () => {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(getUserById(2));
+    const [user, setUser] = useState({
+        firstNAme: "",
+        lastName: "",
+        email: "",
+        password: "",
     })
+
+    const [addresses, setAddresses] = useState([
+        {
+            country: "",
+            state: "",
+            street: "'",
+            addressType: "HOME"
+        },
+    ]);
+
+    const handleUserChange = (e) => {
+        const {name, value} = e.target;
+        setUser({...user, [name]: value});
+    }
+
+    const handleAddressChange = (index, e) => {
+        const {name, value} = e.target;
+        const updatedAddresses = {...addresses};
+        updatedAddresses[index] = {...updatedAddresses[index], [name]: value};
+        setAddresses(updatedAddresses);
+    }
+
+    const addAddress = () => {
+        setAddresses([
+            ...addresses,
+            {country: "", state: "", city: "", street: "", addressType: "HOME"},
+        ]);
+    }
+
+    const removeAddress = (index) => {
+        const updatedAddresses = addresses.filter((_, i) => i !== index);
+        setAddresses(updatedAddresses);
+    }
     return (
         <div>
 
